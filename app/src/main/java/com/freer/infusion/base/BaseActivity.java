5937fba3cfce487da9b04672d5e0eb21248dbb68
Sysia <610372768@qq.com>
2016-05-10 13:46:27 +0800

    create repository

---
+++ app/src/main/java/com/freer/infusion/base/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.freer.infusion.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 2172980000774 on 2016/5/10.
+ */
+public class BaseActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/freer/infusion/base/BaseFragment.java
@@ -0,0 +1,9 @@
+package com.freer.infusion.base;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by 2172980000774 on 2016/5/10.
+ */
+public class BaseFragment extends Fragment {
+}
